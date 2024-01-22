e68dcf695f4024a411bb6d674421d95940609b07
MohammedAldaif <mohammedaldaif71@gmail.com@example.com>
2024-01-21 22:21:46 -0800

    m

---
+++ 2-selection_sort.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "sort.h"
+
+/**
+ * selection_sort - function
+ * @array:input
+ * @size:input
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, k;
+	int temp;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		size_t iMin = i;
+
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[iMin])
+				iMin = j;
+		}
+
+		temp = array[i];
+		array[i] = array[iMin];
+		array[iMin] = temp;
+
+		for (k = 0; k < size; k++)
+		{
+			printf("%d", array[k]);
+			if (k < size - 1)
+				printf(", ");
+		}
+		printf("\n");
+	}
+}
